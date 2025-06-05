8b23aeac0d9e9763cf1aa84600bd46b4f78484f4
Gabriel <gabrielvazcancadoferreira@gmail.com>
2025-06-05 22:22:51 +0200

    Modelei o jogo como uma classe e criei uma função que retorna o jogador vencedor, podendo ser 0 ou 1

--- gameModelling/game.py
+++ gameModelling/game.py
@@ -1,11 +1,77 @@
-
+import random
 
 
 
 class Game:
 
-    def __init__(self):
+    def __init__(self, player1, player2, _debug):
 
-        self.player1 = None
-        self.player2 = None
+        self.debug = _debug
+
+        self.players = {1:player1,2:player2}
+
+        self.round = 1
+
+        self.stairs = {3:16, 5:7,15:25, 18:20, 21:32} # Key represents the base and values the top of the stair
+        self.stairs_base = list(self.stairs.keys())
+
+        self.snakes = {12:2, 14:11, 17:4, 31:19, 35:22} # Key represents head and values the tail of the snake
+        self.snakes_head = list(self.snakes.keys())
+
         pass
+
+    def throw_dice(self):
+
+        return random.randint(1,6)
+    
+    def log(self, message):
+        if self.debug:
+            print(message)
+
+    
+    def play_round(self):
+        
+        self.log(f'Round {self.round} \n')
+
+        for player,player_position in self.players.items():
+
+            self.log(f'Player {player} at position {player_position} \n')
+
+            dice_result = self.throw_dice()
+
+            self.log(f'Dice result for player {player}: {dice_result} \n')
+
+            self.players[player] += dice_result
+
+            self.check_stairs_and_snakes(player)
+
+            if self.players[player] >= 36:
+
+                self.log(f'Player {player} Wins \n')
+
+                return player
+            
+            self.log(f'Player {player} goes to position {self.players[player]} \n')
+
+        return 0
+    
+    
+
+    def check_stairs_and_snakes(self, player):
+
+        if self.players[player] in self.stairs_base:
+
+            self.log(f'Player {player} will climb a stair from {self.players[player]} to {self.stairs[self.players[player]]}\n')
+
+            self.players[player] = self.stairs[self.players[player]]
+            
+        elif self.players[player] in self.snakes_head:
+            
+            self.log(f'Player {player} fell into a snake and will go to position {self.snakes[self.players[player]]} \n')
+
+            self.players[player] = self.snakes[self.players[player]]
+
+        return None
+
+
+
---
+++ gameModelling/runGame.py
@@ -0,0 +1,22 @@
+from game import Game
+
+
+def runGame(debug):
+
+    game = Game(1,1, debug)
+
+    winner = 0
+
+    while winner == 0:
+
+        winner = game.play_round()
+
+        game.round += 1
+
+    print(f'Player {winner} Wins')
+
+    return None
+
+if __name__ == '__main__':
+
+    runGame(False)
